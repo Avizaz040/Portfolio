9c499546d3133b23a071328d77be0e96d574db55
Avishek Mondal <avizaz040@gmail.com>
2025-01-01 01:48:24 +0530

    responsive update number 3

--- src/components/Navlist.jsx
+++ src/components/Navlist.jsx
@@ -13,64 +13,64 @@ function Navlist() {
       transition={{ duration: 1 }}
     >
       <motion.div
-        className="w-[8rem] h-[8rem] md:w-[12rem] md:h-[12rem] lg:w-[16rem] lg:h-[16rem] rounded-full bg-white text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-white-glow cursor-pointer"
+        className="w-[6rem] h-[6rem] sm:w-[8rem] sm:h-[8rem] md:w-[12rem] md:h-[12rem] lg:w-[16rem] lg:h-[16rem] rounded-full bg-white text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-white-glow cursor-pointer p-2"
         initial={{ scale: 0}}
         animate={{ scale: 1 }}
         transition={{ duration: 0.5 }}
         
         onClick={() => navigate("/whyme")}
       >
-        <p className="text-xl md:text-2xl font-semibold">Why me?</p>
+        <p className="text-sm md:text-2xl font-semibold">Why me?</p>
       </motion.div>
       <motion.div
-        className="absolute w-[7rem] h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-gradient-to-r from-cyan-500 to-sky-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-sky-glow cursor-pointer top-[40%] left-[12%] md:top-[36%] md:left-[15%] lg:top-[24%] lg:left-[28%]"
+        className="absolute w-[5rem] h-[5rem] sm:w-[7rem] sm:h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-gradient-to-r from-cyan-500 to-sky-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-sky-glow cursor-pointer top-[40%] left-[12%] sm:top-[40%] sm:left-[20%] md:top-[36%] md:left-[15%] lg:top-[28%] lg:left-[18%] xl:top-[30%] xl:left-[27%] p-2"
         initial={{ scale: 0}}
         animate={{ scale: 1}}
         transition={{ duration: 0.5, delay: 0.2 }}
         
         onClick={() => navigate("/about")}
       >
-        <p className="text-xl md:text-2xl font-semibold text-white">About me</p>
+        <p className="text-sm md:text-2xl font-semibold text-white">About me</p>
       </motion.div>
       <motion.div
-        className="absolute w-[7rem] h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-teal-700 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-green-glow cursor-pointer top-[32%]  md:top-[22%] lg:top-[3%]"
+        className="absolute w-[5rem] h-[5rem] sm:w-[7rem] sm:h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-teal-700 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-green-glow cursor-pointer top-[32%] sm:top-[30%]  md:top-[21%] lg:top-[12%] xl:top-[3%] p-2"
         initial={{ scale: 0}}
         animate={{ scale: 1}}
         transition={{ duration: 0.5, delay: 0.2 }}
         
         onClick={() => navigate("/contact")}
       >
-        <p className="text-xl md:text-2xl font-semibold text-white">Contact me</p>
+        <p className="text-sm  md:text-2xl font-semibold text-white">Contact me</p>
       </motion.div>
       <motion.div
-        className="absolute w-[7rem] h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-green-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-green-glow cursor-pointer top-[40%] right-[12%] md:top-[36%] md:right-[15%] lg:top-[24%] lg:right-[28%]"
+        className="absolute w-[5rem] h-[5rem] sm:w-[7rem] sm:h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-green-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-green-glow cursor-pointer top-[40%] right-[12%] sm:top-[40%] sm:right-[20%] md:top-[36%] md:right-[15%] lg:top-[28%] lg:right-[18%] xl:top-[30%] xl:right-[27%] p-2"
         initial={{ scale: 0}}
         animate={{ scale: 1}}
         transition={{ duration: 0.5, delay: 0.4 }}
         
         onClick={() => navigate("/education")}
       >
-        <p className="text-xl md:text-2xl font-semibold text-white">Education</p>
+        <p className="text-sm  md:text-2xl font-semibold text-white">Education</p>
       </motion.div>
       <motion.div
-        className="absolute w-[7rem] h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-red-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-red-glow cursor-pointer bottom-[36%] left-[18%] md:bottom-[26%] md:left-[23%] lg:bottom-[12%] lg:left-[32%]"
+        className="absolute w-[5rem] h-[5rem] sm:w-[7rem] sm:h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-red-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-red-glow cursor-pointer bottom-[36%] left-[18%] sm:bottom-[32%] sm:left-[28%] md:bottom-[26%] md:left-[23%] lg:bottom-[18%] lg:left-[25%] xl:bottom-[11%] xl:left-[32%] p-2"
         initial={{ scale: 0}}
         animate={{ scale: 1}}
         transition={{ duration: 0.5, delay: 0.6 }}
 
         onClick={() => navigate("/skill")}
       >
-        <p className="text-xl md:text-2xl font-semibold text-white">Skills</p>
+        <p className="text-sm  md:text-2xl font-semibold text-white">Skills</p>
       </motion.div>
       <motion.div
-        className="absolute w-[7rem] h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-pink-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-pink-glow cursor-pointer bottom-[36%] right-[18%] md:bottom-[26%] md:right-[23%] lg:bottom-[12%] lg:right-[32%]"
+        className="absolute w-[5rem] h-[5rem] sm:w-[7rem] sm:h-[7rem] md:w-[10rem] md:h-[10rem] lg:w-[12rem] lg:h-[12rem] rounded-full bg-pink-500 text-center content-center transition-shadow duration-300 ease-in-out shadow-lg shadow-black hover:shadow-pink-glow cursor-pointer bottom-[36%] right-[18%] sm:bottom-[32%] sm:right-[28%] md:bottom-[26%] md:right-[23%] lg:bottom-[18%] lg:right-[25%] xl:bottom-[11%] xl:right-[32%] p-1"
         initial={{ scale: 0}}
         animate={{ scale: 1}}
         transition={{ duration: 0.5, delay: 0.8 }}
         
         onClick={() => navigate("/experience")}
       >
-        <p className="text-xl md:text-2xl font-semibold text-white">Experience</p>
+        <p className="text-sm md:text-2xl font-semibold text-white">Experience</p>
       </motion.div>
     </motion.div>
   );
