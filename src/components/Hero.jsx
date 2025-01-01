2bd9ab09d3912e63eb3919cdf4ca02cf82e0a40a
Avishek Mondal <avizaz040@gmail.com>
2025-01-01 20:48:20 +0530

    home screen update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import AuroraBackgroundDisplay from './AuroraBackgroundDisplay'
 
 function Hero() {
   return (
-    <div className='max-h-screen bg-black text-white flex items-center justify-center'>
+    <div className='max-h-screen w-full bg-black text-white flex items-center justify-center'>
       <AuroraBackgroundDisplay />
     </div>
   )
