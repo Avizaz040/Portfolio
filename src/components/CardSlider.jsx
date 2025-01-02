fa0f7230a0e9b4bc459b37d3d55feeaa0721e3f9
Avishek Mondal <avizaz040@gmail.com>
2025-01-02 11:54:39 +0530

    animation update in cardslider.jsx

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
+import { motion } from 'framer-motion';
 
 
 const skills = [
@@ -68,18 +68,21 @@ const CardSlider = () => {
   };
 
   return (
-    <div className="w-full max-w-4xl mx-auto pb-4 mb-8 px-4">
+    <motion.div className="w-full max-w-4xl mx-auto pb-4 mb-8 px-4"
+      initial={{ scale: 0}}
+      animate={{ scale: 1 }}
+      transition={{ duration: 0.6, delay: 0.5 }}>
       <Slider {...settings}>
         {skills.map((skill, index) => (
           <div key={index} className={`p-8 h-[28rem] sm:h-[24rem] md:h-[25rem] content-center transform transition-transform duration-300 ${index === skillIndex ? 'scale-125 rounded-2xl ' : 'scale-100'} `}>
-            <div className="card bg-white rounded-lg shadow-lg shadow-black p-6 min-h-[20rem] sm:min-h-[16rem] md:min-h-[20rem]  flex flex-col justify-evenly ">
+            <div className="card bg-white rounded-lg shadow-lg shadow-black p-6 min-h-[20rem] sm:min-h-[16rem] md:min-h-[20rem] flex flex-col justify-evenly ">
               <h3 className="text-2xl font-bold mb-2">{skill.title}</h3>
               <p className="text-gray-700">{skill.description}</p>
             </div>
           </div>
         ))}
       </Slider>
-    </div>
+    </motion.div>
   );
 };
 
